28d8a9fd65798736f2ef89ba933e232ab4924b23
Nicolas Frankel <nicolas@frankel.ch>
2012-10-19 12:23:45 +0200

    Added text field and button that display field value

--- jugdemo/src/main/java/com/morevaadin/vaadin7/jugdemo/DemoUi.java
+++ jugdemo/src/main/java/com/morevaadin/vaadin7/jugdemo/DemoUi.java
@@ -1,8 +1,12 @@
 package com.morevaadin.vaadin7.jugdemo;
 
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 
 @SuppressWarnings("serial")
@@ -12,11 +16,27 @@ public class DemoUi extends UI {
 	protected void init(VaadinRequest request) {
 
 		HorizontalLayout layout = new HorizontalLayout();
-		
-		Label label = new Label("Hello JUG!");
-		
+
+		final TextField text = new TextField("", "JUG");
+
+		Button button = new Button("Say Hello");
+
+		final Label label = new Label();
+
+		button.addClickListener(new ClickListener() {
+
+			public void buttonClick(ClickEvent event) {
+
+				String value = text.getValue();
+
+				label.setValue("Hello " + value + "!");
+			}
+		});
+
+		layout.addComponent(text);
+		layout.addComponent(button);
 		layout.addComponent(label);
-		
+
 		setContent(layout);
 	}
 }
