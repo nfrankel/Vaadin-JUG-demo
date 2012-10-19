3737178834e23a28edf82f690594666c2d67fb0c
Nicolas Frankel <nicolas@frankel.ch>
2012-10-19 12:35:37 +0200

    Created a view and added a display to display it

--- jugdemo/src/main/java/com/morevaadin/vaadin7/jugdemo/DemoUi.java
+++ jugdemo/src/main/java/com/morevaadin/vaadin7/jugdemo/DemoUi.java
@@ -1,13 +1,8 @@
 package com.morevaadin.vaadin7.jugdemo;
 
+import com.vaadin.navigator.Navigator.SimpleViewDisplay;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Button.ClickListener;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
 
 @SuppressWarnings("serial")
 public class DemoUi extends UI {
@@ -15,31 +10,10 @@ public class DemoUi extends UI {
 	@Override
 	protected void init(VaadinRequest request) {
 
-		VerticalLayout layout = new VerticalLayout();
-
-		layout.setMargin(true);
-		layout.setSpacing(true);
-
-		final TextField text = new TextField("", "JUG");
-
-		Button button = new Button("Say Hello");
-
-		final Label label = new Label();
-
-		button.addClickListener(new ClickListener() {
-
-			public void buttonClick(ClickEvent event) {
-
-				String value = text.getValue();
-
-				label.setValue("Hello " + value + "!");
-			}
-		});
-
-		layout.addComponent(text);
-		layout.addComponent(button);
-		layout.addComponent(label);
-
-		setContent(layout);
+		SimpleViewDisplay display = new SimpleViewDisplay();
+		
+		display.showView(new MainView());
+		
+		setContent(display);
 	}
 }
---
+++ jugdemo/src/main/java/com/morevaadin/vaadin7/jugdemo/MainView.java
@@ -0,0 +1,47 @@
+package com.morevaadin.vaadin7.jugdemo;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+
+@SuppressWarnings("serial")
+public class MainView extends CustomComponent implements View {
+
+	public MainView() {
+		
+		VerticalLayout layout = new VerticalLayout();
+
+		layout.setMargin(true);
+		layout.setSpacing(true);
+
+		final TextField text = new TextField("", "JUG");
+
+		Button button = new Button("Say Hello");
+
+		final Label label = new Label();
+
+		button.addClickListener(new ClickListener() {
+
+			public void buttonClick(ClickEvent event) {
+
+				String value = text.getValue();
+
+				label.setValue("Hello " + value + "!");
+			}
+		});
+
+		layout.addComponent(text);
+		layout.addComponent(button);
+		layout.addComponent(label);
+
+		setCompositionRoot(layout);
+	}
+	
+	public void enter(ViewChangeEvent event) {}
+}
