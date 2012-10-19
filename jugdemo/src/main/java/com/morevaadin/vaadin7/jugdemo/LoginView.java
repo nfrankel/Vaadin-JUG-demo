2f706bec406568d82d30e656aeb85ab21a5ca790
Nicolas Frankel <nicolas@frankel.ch>
2012-10-19 16:04:55 +0200

    Added login view and feature

---
+++ jugdemo/src/main/java/com/morevaadin/vaadin7/jugdemo/LoginView.java
@@ -0,0 +1,48 @@
+package com.morevaadin.vaadin7.jugdemo;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.PasswordField;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+
+@SuppressWarnings("serial")
+public class LoginView extends CustomComponent implements View {
+
+	private TextField loginField = new TextField("Login", "juguser");
+
+	private PasswordField passwordField = new PasswordField("Password");
+
+	public LoginView() {
+
+		FormLayout layout = new FormLayout();
+
+		layout.setMargin(true);
+		layout.setSpacing(true);
+
+		setCompositionRoot(layout);
+
+		layout.addComponent(loginField);
+		layout.addComponent(passwordField);
+
+		Button button = new Button("Enter");
+
+		button.addClickListener(new ClickListener() {
+
+			public void buttonClick(ClickEvent event) {
+
+				((DemoUi) UI.getCurrent()).login(loginField.getValue(),
+						passwordField.getValue());
+			}
+		});
+
+		layout.addComponent(button);
+	}
+
+	public void enter(ViewChangeEvent event) {}
+}
