e8e4c0dca361da91b9976db1390ef5e65932ee22
Nicolas Frankel <nicolas@frankel.ch>
2012-10-19 10:32:36 +0200

    Initial projet creation

--- jugdemo/src/main/java/com/morevaadin/vaadin7/jugdemo/DemoUi.java
+++ jugdemo/src/main/java/com/morevaadin/vaadin7/jugdemo/DemoUi.java
@@ -1,15 +1,15 @@
 package com.morevaadin.vaadin7.jugdemo;
 
-import com.vaadin.terminal.WrappedRequest;
+import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
-import com.vaadin.ui.Root;
+import com.vaadin.ui.UI;
 
 @SuppressWarnings("serial")
-public class DemoRoot extends Root {
+public class DemoUi extends UI {
 
 	@Override
-	protected void init(WrappedRequest request) {
+	protected void init(VaadinRequest request) {
 
 		HorizontalLayout layout = new HorizontalLayout();
 		
